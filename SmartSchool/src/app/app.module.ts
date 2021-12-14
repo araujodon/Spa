3e3616526d2f4415476f2bac0b4450e4d7e38a78
ViniCSena <cardosovinicius32@gmail.com>
2021-12-13 22:34:35 -0300

    Spa

---
+++ SmartSchool/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { ProfessoresComponent } from './professores/professores.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    NavComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
